23d367d1b7ad57aba80d7e31b7863eb542177b60
hassan <hs9253286@gmail.com>
2023-07-31 13:08:41 +0500

    change myslef

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ print(c-d)
 e=3
 f=7
 print(e*f)
-r=3
+qw=3
 t=4
-print(r**4)
+print(qw**t)
 print(" hi this is hassan shah i wnat to show you something ")
 print("my intrested in data scicene field")
 print(" my age is 21 and i am study in riphah university ")
